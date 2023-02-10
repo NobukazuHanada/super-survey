1543838185049430a0b6f00e1669c661cf5f3e56
nobkz <tone.nobukazu@gmail.com>
2023-02-10 19:42:39 +0900

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,3 @@
-import './style.css'
-
-const SVG_NS = "http://www.w3.org/2000/svg";
-
-function createSVG(name: string): SVGElement {
-  return document.createElementNS(SVG_NS, name);
-}
-function setSVGAttributes(elm: SVGElement, attributes: { [key in string]: string }) {
-  for (let [key, value] of Object.entries(attributes)) {
-    elm.setAttribute(key, value);
-  }
-}
+import "./style.css";
 
 const surveyBoardElm = document.getElementById("survey-board")!;
-const surveryBoardSvg = createSVG("svg");
-setSVGAttributes(surveryBoardSvg, {
-  width: "500",
-  height: "500",
-  viewBox: "0 0 200 200",
-});
-
-
-surveyBoardElm.appendChild(surveryBoardSvg);
-const rect = createSVG("rect");
-surveryBoardSvg.appendChild(rect);
-setSVGAttributes(rect, {
-  x: "100",
-  y: "100",
-  width: "100",
-  height: "100"
-})
