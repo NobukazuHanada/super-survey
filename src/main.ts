bb32ea0d8b90d30e2d35f163ddf7adb7f75c8e47
Nobkz <tone.nobukazu@gmail.com>
2023-02-13 18:16:03 +0900

    build

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,10 @@
 import "./style.css";
+import { RectObject, SVGObject } from "./svg-object";
 
 const surveyBoardElm = document.getElementById("survey-board")!;
+const svg = SVGObject.svg();
+svg.mount(surveyBoardElm);
+
+svg.addChild(
+  new RectObject(10, 10, 100, 100, { fill: "#fff", stroke: "#ccc", rx: 10 })
+);
--- src/svg-object.ts
+++ src/svg-object.ts
@@ -1,8 +1,10 @@
 import { createSVG, getSVGAttribute, setSVGAttribute } from "./svg-util";
 
+export type color = string;
+
 export class SVGObject {
   element: SVGElement;
-  parent: SVGObject | undefined;
+  parent: SVGObject | undefined | null;
   children: SVGObject[];
 
   static svg(): SVGObject {
@@ -14,10 +16,20 @@ export class SVGObject {
     this.children = [];
   }
 
+  mount(elm: Element) {
+    elm.appendChild(this.element);
+  }
+
   addChild(child: SVGObject) {
     this.element.appendChild(child.element);
     this.children.push(child);
     child.parent = this;
+  }
+
+  removeChild(child: SVGObject) {
+    this.element.removeChild(child.element);
+    this.children.splice(this.children.indexOf(child));
+    child.parent = null;
   }
 
   setAttribute(name: string, value: string) {
@@ -27,4 +39,47 @@ export class SVGObject {
   getAttribute(name: string): string | null {
     return getSVGAttribute(this.element, name);
   }
+
+  setFill(color: color) {
+    this.setAttribute("fill", color);
+  }
+
+  setStroke(color: color) {
+    this.setAttribute("stroke", color);
+  }
+}
+
+export class RectObject extends SVGObject {
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+  constructor(x: number, y: number, width: number, height: number, option?: { fill?: color, stroke?: color, rx?: number }) {
+    super("rect");
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.setAttribute("x", x.toString());
+    this.setAttribute("y", y.toString());
+    this.setAttribute("width", width.toString());
+    this.setAttribute("height", height.toString());
+    if (option?.rx) {
+      this.setAttribute("rx", option.rx);
+    }
+    if (option?.fill) {
+      this.setFill(option.fill);
+    }
+    if (option?.stroke) {
+      this.setStroke(option.stroke);
+    }
+  }
+
+  move(x: number, y: number) {
+    this.x += x;
+    this.y += y;
+    this.setAttribute("x", this.x.toString());
+    this.setAttribute("y", this.y.toString());
+  }
+
 }
